eab18f9f3e6c615c1a990e488bf588132b5767d5
Alex Vilchis <alexrv4299@gmail.com>
2019-11-06 15:38:45 -0600

    Comment out LCD

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,23 +1,25 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+//LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 
-const int pinA = 3;
-const int pinB = 4;
-const int pinC = 5;
-const int pinD = 6;
+const int pinA = 11;
+const int pinB = 10;
+const int pinC = 7;
+const int pinD =  6;
+const int pinMotor = 9;
 
 
 
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16, 2);
+//  lcd.begin(16, 2);
 
   pinMode(pinA, INPUT);
   pinMode(pinB, INPUT);
   pinMode(pinC, INPUT);
   pinMode(pinD, INPUT);
+  analogWrite(pinMotor, 80);
 
 }
 
@@ -27,12 +29,19 @@ void loop() {
   if (digitalRead(pinB)) suma += 2;
   if (digitalRead(pinC)) suma += 4;
   if (digitalRead(pinD)) suma += 8;
-  lcd.setCursor(0, 0);
-  lcd.print("Suma: ");
-  lcd.print(suma);
+  //lcd.setCursor(0, 0);
+  //lcd.print("Suma: ");
+  //lcd.print(suma);
 
-  lcd.setCursor(0, 1);
-  lcd.print("Grados: ");
-  lcd.print(map(suma, 0, 15, 0, 360));
-  delay(10);
+  //lcd.setCursor(0, 1);
+//  lcd.print("Grados: ");
+//  lcd.print(map(suma, 0, 15, 0, 360));
+
+  Serial.print("Suma: ");
+  Serial.println(suma);
+  Serial.print("Grados: ");
+  Serial.println(map(suma, 0, 15, 0, 360));
+
+
+  delay(100);
 }
